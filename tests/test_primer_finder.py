e9c95bae60a676c6b2a409cfcdc694e9086fe767
Mkongamawe <clement.mwagwabi@outlook.com>
2026-05-19 19:39:32 +0300

    format update

--- tests/test_primer_finder.py
+++ tests/test_primer_finder.py
@@ -5,7 +5,7 @@ from genomekit import GenomeKit
 
 def happy_sequence():
     random.seed(26)
-    dna = ''.join(random.choices(["a", "c", "t", "g"], k = 50))
+    dna = "".join(random.choices(["a", "c", "t", "g"], k=50))
     dna = GenomeKit(dna)
     assert not dna.find_primers().forward.verdict
     assert not dna.find_primers().reverse.verdict
